95d5e76ee8466b5a641d2f10edf24db918763ce3
Jan Van den bosch <jan@bossie.org>
2017-01-10 16:24:08 +0100

    use catalogclient to fetch files

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup
 
 setup(name='python-spark-quickstart',
       version='0.1',
-      packages=['histogram'])
+      packages=['histogram'],
+      install_requires=['rasterio', 'numpy', 'catalogclient'])
--- spark.py
+++ spark.py
@@ -1,23 +1,34 @@
 """spark.py"""
 
+from datetime import datetime
+from catalogclient.catalog import Catalog
 from histogram.histogram import histogram
 from pyspark import SparkContext
 
 
-def files():
-    return ['/data/MTDA/TIFFDERIVED/PROBAV_L3_S1_TOC_333M/20160424/PROBAV_S1_TOC_20160424_333M_V001/PROBAV_S1_TOC_X35Y12_20160424_333M_V001_NDVI.tif']
-
-
-def as_string(hist):
+def join(hist):
     return " ".join([str(i) for i in hist])
 
+
+def ndvi_files(products):
+    prefix = 'file:'
+
+    return [file.filename[len(prefix):] for product in products for file in product.files if
+     file.filename.startswith(prefix) and 'NDVI' in file.bands]
+
+
+products = Catalog().get_products('PROBAV_L3_S5_TOC_100M', 'GEOTIFF',
+                                  startdate=datetime(2016, 1, 1), enddate=datetime(2016, 2, 1),
+                                  min_lon=2.5, max_lon=6.5, min_lat=49.5, max_lat=51.5)
+
+files = ndvi_files(products)
 
 sc = SparkContext('local', 'python-spark-quickstart')
 
 try:
-    hists = sc.parallelize(files()).map(histogram)
+    hists = sc.parallelize(files).map(histogram)
 
-    for hist in map(as_string, hists.collect()):
+    for hist in map(join, hists.collect()):
         print hist
 finally:
     sc.stop()
